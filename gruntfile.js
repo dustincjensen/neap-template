962add7cdf93a355dad846c6d3b4d6818e0ffb0c
Dustin Jensen <dustincjensen@gmail.com>
2017-05-18 22:00:37 -0600

    Adds Angular (2^) - configures gruntfile to serve up the specified angular dependencies to dist/public/lib. - system.config.js is copied to dist/public and handles the module loading for Angular2.

--- gruntfile.js
+++ gruntfile.js
@@ -16,6 +16,59 @@ module.exports = function(grunt) {
             dest: "./dist/public"
           }
         ]
+      },
+      lib: {
+        files: [
+          {
+            expand: true,
+            cwd: "./node_modules/",
+            src: [
+              "core-js/client/shim.min.js*",
+              "zone.js/dist/zone.js",
+              "rxjs/bundles/Rx.min.js",
+              "rxjs/Observable.js*",
+              "rxjs/observable/merge.js*",
+              "rxjs/operator/share.js*",
+              "rxjs/Subject.js*",
+              "rxjs/util/root.js*",
+              "rxjs/util/toSubscriber.js*",
+              "rxjs/symbol/observable.js*",
+              "rxjs/operator/merge.js*",
+              "rxjs/operator/multicast.js*",
+              "rxjs/Subscriber.js*",
+              "rxjs/Subscription.js*",
+              "rxjs/util/ObjectUnsubscribedError.js*",
+              "rxjs/SubjectSubscription.js*",
+              "rxjs/symbol/rxSubscriber.js*",
+              "rxjs/Observer.js*",
+              "rxjs/observable/ArrayObservable.js*",
+              "rxjs/operator/mergeAll.js*",
+              "rxjs/util/isScheduler.js*",
+              "rxjs/observable/ConnectableObservable.js*",
+              "rxjs/util/isFunction.js*",
+              "rxjs/util/isArray.js*",
+              "rxjs/util/isObject.js*",
+              "rxjs/util/tryCatch.js*",
+              "rxjs/util/errorObject.js*",
+              "rxjs/util/UnsubscriptionError.js*",
+              "rxjs/observable/ScalarObservable.js*",
+              "rxjs/observable/EmptyObservable.js*",
+              "rxjs/OuterSubscriber.js*",
+              "rxjs/util/subscribeToResult.js*",
+              "rxjs/util/isArrayLike.js*",
+              "rxjs/util/isPromise.js*",
+              "rxjs/symbol/iterator.js*",
+              "rxjs/InnerSubscriber.js*",
+              "@angular/core/bundles/core.umd.js*",
+              "@angular/common/bundles/common.umd.js*",
+              "@angular/compiler/bundles/compiler.umd.js*",
+              "@angular/platform-browser/bundles/platform-browser.umd.js*",
+              "@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js*",
+              "systemjs/dist/system.src.js*",
+            ],
+            dest: "./dist/public/lib"
+          }
+        ]
       }
     },
     ts: {
@@ -27,7 +80,8 @@ module.exports = function(grunt) {
         options: {
           module: "commonjs",
           target: "es6",
-          sourceMap: false
+          sourceMap: false,
+          experimentalDecorators: true
         }
       }
     },
@@ -52,8 +106,8 @@ module.exports = function(grunt) {
         tasks: ["sass"]
       },
       public: {
-        files: ["src/\*\*/\*.html"],
-        tasks: ["copy"]
+        files: ["src/\*\*/\*.html", "src/\*\*/\*.js"],
+        tasks: ["copy:build"]
       }
     }
   });
@@ -63,8 +117,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-ts");
   grunt.loadNpmTasks("grunt-sass");
   grunt.registerTask("default", [
-    "copy",
+    "copy:build",
     "ts",
     "sass"
+  ]);
+  grunt.registerTask("copy-lib", [
+    "copy:lib"
   ]);
 };
