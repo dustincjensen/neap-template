5482be86bb2491e7635e8dc8d293d2fd301f5dea
Dustin Jensen <dustincjensen@gmail.com>
2017-07-21 22:04:20 -0600

    Fills out most of the form with the language template to see how it works.

--- src/public/app/i18n/languages/fr.ts
+++ src/public/app/i18n/languages/fr.ts
@@ -11,10 +11,30 @@ export let French: Language = {
     Example: {
         Title: (count: number, greeting: string) => {
             return `Exemple de page ${count} ${greeting}`;
-        }
+        },
+        CakeQuestion: 'Voulez-vous du gâteau?',
+        IceCreamQuestion: 'Qu\'en est-il de la glace?'
     },
     Common: {
+        Name: 'le nom',
+        Year: 'An',
+        Description: 'La description',
+
+        NameRequired: 'Vous devez fournir un nom.',
+        YearMustBeBetween: (year: number, maxYear: number) => {
+            return `Vous devez fournir un an entre ${year} et ${maxYear}.`;
+        },
+        StringMaxLength: (maxLength: number) => {
+            return `La longueur maximale autorisée est de ${maxLength}.`;
+        },
+
+        GenericWarning: 'Nous avons un problème et c\'est l\'avertissement.',
+        NoRecordsToShow: 'Aucun enregistrement à afficher',
+
+        NewButton: 'Nouveau',
         UpdateButton: 'Mettre à jour',
-        DeleteButton: 'Effacer'
+        DeleteButton: 'Effacer',
+        SubmitButton: 'Soumettre',
+        CancelButton: 'Annuler'
     }
 }
