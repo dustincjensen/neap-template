4c78b5548d79f953161ea02c4b6142f60cc7a160
Dustin Jensen <dustincjensen@gmail.com>
2017-08-06 01:49:57 -0600

    Adds <T extends typeof ApiExternal> - This makes it so the decorators can only be used on classes that extend the ApiExternal and it gives us type helping through target.prototype to identify what fields we want to set.

--- src/api-external/externalDecorators.ts
+++ src/api-external/externalDecorators.ts
@@ -2,38 +2,45 @@ import { ApiExternal } from './apiExternal';
 import { Http } from '../utils/server/http';
 
 /**
- * 
+ * If your external api uses https instead of http
+ * then you should include this decorator.
  */
-export function https() {
-    return function (target: Function) {
+export function https<T extends typeof ApiExternal>() {
+    return function (target: T) {
         target.prototype.https = true;
     }
 }
 
 /**
- * 
- * @param host 
+ * To set the host name of the external api, use this decorator.
+ * An example of a host name would be, 'api.github.com' without
+ * the https or http.
+ * @param host the web address of the external api.
  */
-export function host(host: string) {
-    return function (target: Function) {
+export function host<T extends typeof ApiExternal>(host: string) {
+    return function (target: T) {
         target.prototype.host = host;
     }
 }
 
 /**
- * 
- * @param obj 
+ * If you need to set specific headers for all the calls then you
+ * can set them here.
+ * An example would be, { 'user-agent': 'programName' }
+ * @param obj the object that defines the list of headers that should be included.
  */
-export function headers(obj: Object) {
-    return function (target: Function) {
+export function headers<T extends typeof ApiExternal>(obj: Object) {
+    return function (target: T) {
         target.prototype.headers = obj;
     }
 }
 
 /**
- * 
- * @param path 
- * @param type 
+ * For individual method calls.
+ * This allows you set the path for the api call and the type of call that should
+ * be executed on your behalf.
+ * @param path the path for the api call.
+ * @param type the type of api call to do.
  */
 export function path(path: string, type: Http.Type) {
     return function (target: ApiExternal, name: string, methodDescriptor: TypedPropertyDescriptor<(...args: any[]) => Promise<(payload: any) => Promise<any>>>) {
